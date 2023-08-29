b7bbf224d7f83d8ca159e7969a69b10e58725c39
Mike Leon <>
2023-08-29 15:50:09 -0500

    .

--- index.js
+++ index.js
@@ -64,6 +64,12 @@ const partnerSchema = new Schema({
         // const mongoDB = process.env.DB_CONNECTION_STRING;
         const mongoDB = "mongodb+srv://admin:P%23ssword1212@relationshipcluster.k27hm3q.mongodb.net/Relationships";
          mongoose.connect(mongoDB);
+         console.log('try to person found');
+        const person = CustomerModel.findOne({ last: 'Leon' }).exec();
+        console.log('person found');
+        console.log('name is '+ person);
+        PartnerModel.create({ customer:person.id, name: 'Linda Smith', DOB:'1/1/2000', DateStarted: '3/2/2022', DateEnded:'3/3/2022'});
+        console.log('smith created');
          console.log('connected to mongo');
         console.log("start listening");
         app.listen(process.env.PORT || 3000,() => {
